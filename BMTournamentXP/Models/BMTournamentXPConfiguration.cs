987b8b9ab517bb289458cc38f392b3c875f78382
Brandon Mathis <brandon@darkspyre.org>
2020-04-04 01:50:53 -0500

    Fixed crash on map when generating tournament items. Added Extra cash to tournament winnings in TweakCompilation

--- BMTournamentXP/Models/BMTournamentXPConfiguration.cs
+++ BMTournamentXP/Models/BMTournamentXPConfiguration.cs
@@ -11,12 +11,13 @@ namespace BMTournamentXP.Models
 {
     public class BMTournamentXPConfiguration
     {
-        
+
         public BMTournamentXPConfiguration(string xmlpath = "")
         {
             if (xmlpath != "")
             {
                 XmlDocument xmlDocument = new XmlDocument();
+
                 xmlDocument.Load(xmlpath);
                 XmlNode xmlNodes = xmlDocument.SelectSingleNode("BMTournamentXP");
                 foreach (XmlNode n in xmlNodes.ChildNodes)
@@ -56,7 +57,7 @@ namespace BMTournamentXP.Models
                                     case "additionalgold":
                                         int bg = 0;
                                         int.TryParse(nc.InnerText, out bg);
-                                        //TournamentBehaviourPatch.bonusmoney = bg;
+                                        BMTournamentXPMain.Configuration.TournamentBonusMoney = bg;
                                         break;
                                     case "enablereroll":
                                         if (string.Equals(nc.InnerText, "true", StringComparison.OrdinalIgnoreCase))
@@ -78,7 +79,7 @@ namespace BMTournamentXP.Models
                                         BMTournamentXPMain.Configuration.TownPrizeMin = int.Parse(nc.InnerText.Trim());
                                         break;
                                     case "includedtownprizetypes":
-                                        foreach(XmlNode np in nc.ChildNodes)
+                                        foreach (XmlNode np in nc.ChildNodes)
                                         {
                                             try
                                             {
@@ -117,13 +118,10 @@ namespace BMTournamentXP.Models
                             }
                             break;
 
-                            //case "maximumtournamentbet":
-                            //    int bet = 300;
-                            //    int.TryParse(n.InnerText, out bet);
-                            //    _maximumTournamentBet = bet;
-                            //    break;                                       
+
                     }
                 }
+                xmlDocument = null;
             }
         }
 
@@ -132,6 +130,7 @@ namespace BMTournamentXP.Models
         public bool IsTournamentXPEnabled { get; set; } = true;
         public float TournamentXPAdjustment { get; set; } = 1.0f;
         public bool TournamentPrizeRerollEnabled { get; set; } = true;
+        public int TournamentBonusMoney { get; set; } = 500;
         public bool IsArenaXPEnabled { get; set; } = true;
         public float ArenaXPAdjustment { get; set; } = 1.0f;
 
@@ -139,7 +138,7 @@ namespace BMTournamentXP.Models
         public int TownPrizeMin { get; set; } = 1000;
         public int TownPrizeMax { get; set; } = 10000;
         public List<ItemObject.ItemTypeEnum> TownValidPrizeTypes { get; set; } = new List<ItemObject.ItemTypeEnum>();
-        
+
         public bool UseTownInventoryAsPrize { get; set; } = true;
         public List<string> TourneyItems = new List<string>() { "winds_fury_sword_t3", "bone_crusher_mace_t3", "tyrhung_sword_t3", "pernach_mace_t3", "early_retirement_2hsword_t3", "black_heart_2haxe_t3", "knights_fall_mace_t3", "the_scalpel_sword_t3", "judgement_mace_t3", "dawnbreaker_sword_t3", "ambassador_sword_t3", "heavy_nasalhelm_over_imperial_mail", "closed_desert_helmet", "sturgian_helmet_closed", "full_helm_over_laced_coif", "desert_mail_coif", "heavy_nasalhelm_over_imperial_mail", "plumed_nomad_helmet", "eastern_studded_shoulders", "ridged_northernhelm", "armored_bearskin", "noble_horse_southern", "noble_horse_imperial", "noble_horse_western", "noble_horse_eastern", "noble_horse_battania", "noble_horse_northern", "special_camel" };
     }
--- BMTournamentXP/Patch/PatchTournamentCampaignBehavior.cs
+++ BMTournamentXP/Patch/PatchTournamentCampaignBehavior.cs
@@ -17,6 +17,8 @@ namespace BMTournamentXP
 {
 
 
+
+
     [HarmonyPatch(typeof(TournamentCampaignBehavior), "AddGameMenus")]
     public class TournamentCampaignBehaviorPatch1
     {
@@ -55,6 +57,8 @@ namespace BMTournamentXP
             typeof(TournamentGame).GetProperty("Prize").SetValue(tournamentGame, prize);
         }
 
+
+
         //private static ItemObject GetTournamentPrize()
         //{
         //    string[] strArray = new String[] { "winds_fury_sword_t3", "bone_crusher_mace_t3", "tyrhung_sword_t3", "pernach_mace_t3", "early_retirement_2hsword_t3", "black_heart_2haxe_t3", "knights_fall_mace_t3", "the_scalpel_sword_t3", "judgement_mace_t3", "dawnbreaker_sword_t3", "ambassador_sword_t3", "heavy_nasalhelm_over_imperial_mail", "closed_desert_helmet", "sturgian_helmet_closed", "full_helm_over_laced_coif", "desert_mail_coif", "heavy_nasalhelm_over_imperial_mail", "plumed_nomad_helmet", "eastern_studded_shoulders", "ridged_northernhelm", "armored_bearskin", "noble_horse_southern", "noble_horse_imperial", "noble_horse_western", "noble_horse_eastern", "noble_horse_battania", "noble_horse_northern", "special_camel" };
@@ -67,71 +71,90 @@ namespace BMTournamentXP
     {
         public GetTournamentPrizePatch1() { }
 
-        public static bool Prefix(ref ItemObject __result)
+        public static bool Prefix(TournamentGame __instance, ref ItemObject __result)
         {
-            List<string> strArray = new List<string>();
-            foreach (var x in BMTournamentXPMain.Configuration.TourneyItems)
-            {
-                strArray.Add(x);
-            }
 
+            List<string> townitems = new List<string>();
             if (BMTournamentXPMain.Configuration.PrizeListMode.Trim().IndexOf("town", StringComparison.OrdinalIgnoreCase) >= 0)
             {
-                //if it's only, remove the stock/custom items
+                townitems = GetValidTownItems(__instance.Town.Owner.ItemRoster, BMTournamentXPMain.Configuration.TownPrizeMin, BMTournamentXPMain.Configuration.TownPrizeMax, BMTournamentXPMain.Configuration.TownValidPrizeTypes);
+
                 if (BMTournamentXPMain.Configuration.PrizeListMode.Trim().IndexOf("only", StringComparison.OrdinalIgnoreCase) >= 0)
                 {
-                    strArray = new List<string>();
-                }
-                //var itemsRos = Hero.MainHero.CurrentSettlement.ItemRoster
-                //Settlement.CurrentSettlement.ItemRoster.RemoveZeroCounts();
-                IMarketData md = (IMarketData)Traverse.Create(InventoryManager.Instance).Method("GetCurrentMarketData").GetValue();
-
-                var roster = Settlement.CurrentSettlement.ItemRoster;
-                roster.RemoveZeroCounts();
-                var list = roster.Where(x =>
-                x.Amount > 0
-                && BMTournamentXPMain.Configuration.TownValidPrizeTypes.Contains(x.EquipmentElement.Item.ItemType)
-               && x.EquipmentElement.Item.Value >= BMTournamentXPMain.Configuration.TownPrizeMin
-               && x.EquipmentElement.Item.Value <= BMTournamentXPMain.Configuration.TownPrizeMax
-                  ).OrderByDescending(x => x.EquipmentElement.Item.Value).Select(x => x.EquipmentElement.Item.StringId).ToList();
-
-                int count = 0;
-                foreach (var s in list)
-                {
-                    strArray.Add(s);
-                    count++;
-                    if (count >= 10)
-                    {
-                        break;
-                    }
+                    __result = Game.Current.ObjectManager.GetObject<ItemObject>(townitems.GetRandomElement<string>());                    
+                    return false;
                 }
             }
-            __result = Game.Current.ObjectManager.GetObject<ItemObject>(strArray.GetRandomElement<string>());
+            //if we are still here, append the town items if any, to the custom/stock list.
+            var allitems = BMTournamentXPMain.Configuration.TourneyItems.Concat(townitems.Take(10)).ToArray();
+            __result = Game.Current.ObjectManager.GetObject<ItemObject>(allitems.GetRandomElement<string>());
+
             return false;
+            //List<string> strArray = new List<string>();
+            //foreach (var x in BMTournamentXPMain.Configuration.TourneyItems)
+            //{
+            //    strArray.Add(x);
+            //}
+
+            //if (BMTournamentXPMain.Configuration.PrizeListMode.Trim().IndexOf("town", StringComparison.OrdinalIgnoreCase) >= 0)
+            //{
+            //    //if it's only, remove the stock/custom items
+            //    if (BMTournamentXPMain.Configuration.PrizeListMode.Trim().IndexOf("only", StringComparison.OrdinalIgnoreCase) >= 0)
+            //    {
+            //        strArray = new List<string>();
+            //    }
+            //    //var itemsRos = Hero.MainHero.CurrentSettlement.ItemRoster
+            //    //Settlement.CurrentSettlement.ItemRoster.RemoveZeroCounts();
+            //    IMarketData md = (IMarketData)Traverse.Create(InventoryManager.Instance).Method("GetCurrentMarketData").GetValue();
+
+            //    var roster = Settlement.CurrentSettlement.ItemRoster;
+            //    roster.RemoveZeroCounts();
+            //    var list = roster.Where(x =>
+            //    x.Amount > 0
+            //    && BMTournamentXPMain.Configuration.TownValidPrizeTypes.Contains(x.EquipmentElement.Item.ItemType)
+            //   && x.EquipmentElement.Item.Value >= BMTournamentXPMain.Configuration.TownPrizeMin
+            //   && x.EquipmentElement.Item.Value <= BMTournamentXPMain.Configuration.TownPrizeMax
+            //      ).OrderByDescending(x => x.EquipmentElement.Item.Value).Select(x => x.EquipmentElement.Item.StringId).ToList();
+
+            //    int count = 0;
+            //    foreach (var s in list)
+            //    {
+            //        strArray.Add(s);
+            //        count++;
+            //        if (count >= 10)
+            //        {
+            //            break;
+            //        }
+            //    }
+            //}
+            //__result = Game.Current.ObjectManager.GetObject<ItemObject>(strArray.GetRandomElement<string>());
+
         }
 
+        static bool Prepare()
+        {
+            if (string.Equals(BMTournamentXPMain.Configuration.PrizeListMode.Trim(), "stock", StringComparison.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static List<string> GetValidTownItems(ItemRoster roster, int minValue, int maxValue, List<ItemObject.ItemTypeEnum> validtypes)
+        {
+            roster.RemoveZeroCounts();
+            var list = roster.Where(x =>
+            x.Amount > 0
+            && validtypes.Contains(x.EquipmentElement.Item.ItemType)
+           && x.EquipmentElement.Item.Value >= minValue
+           && x.EquipmentElement.Item.Value <= maxValue
+              ).OrderByDescending(x => x.EquipmentElement.Item.Value).Select(x => x.EquipmentElement.Item.StringId).ToList();
+            return list;
+        }
+
+       
     }
 
 }
 
 
-
-//class BMTournamentBehavior : TournamentBehavior
-//{
-//    public BMTournamentBehavior(TournamentGame tournamentGame, Settlement settlement, ITournamentGameBehavior gameBehavior, bool isPlayerParticipating)
-//        : base(tournamentGame, settlement, gameBehavior, isPlayerParticipating)
-//    {
-
-//    }
-
-//    new public int MaximumBet = 300;
-
-
-//    new public int MaximumBetInstance
-//    {
-//        get
-//        {
-//            return Math.Min(MaximumBet, this.PlayerDenars);
-//        }
-//    }
-//}
